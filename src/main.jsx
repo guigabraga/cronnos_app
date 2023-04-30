a5979a39da9b2361e2fbb1665e189c23644ed4dc
Guilherme Braga <guilherme_braga100@hotmail.com>
2023-04-30 01:32:04 -0300

    Implementação de novas rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import "./global-style.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+
+import Login from './routes/Login.jsx';
+import Cadastro from './routes/Cadastro.jsx';
+
+const router = createBrowserRouter([
+    {
+        path: "login",
+        element: <Login />
+    },
+    {
+        path: "cadastro",
+        element: <Cadastro /> 
+    }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
