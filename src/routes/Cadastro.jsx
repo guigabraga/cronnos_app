c048a36de2b68b8f7fb71adc273e745b65b1ccce
Guilherme Braga <guilherme_braga100@hotmail.com>
2023-04-30 14:14:48 -0300

    Atualização de componente

--- src/routes/Cadastro.jsx
+++ src/routes/Cadastro.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../style/Cadastro.css";
 import cronnosLogo from "../assets/cronnos-logo.svg";
 
@@ -7,10 +8,10 @@ const Cadastro = () => {
     return(
 
         <div className="componentCadastro">
-            <div className="row align-items-center text-center">
-                <div className="co-xl-6 col-lg-6 col-md-12 col-sm-12 shadow-sm rounded bg-light">
-                    <div className="p-4">
-                        <img src={cronnosLogo} alt="" width="70%" />
+            <div className="row text-center">
+                <div className="co-xl-6 col-lg-6 col-md-12 col-sm-12 shadow-sm rounded bg-light p-5">
+                    <div className="mt-5">
+                        <img src={cronnosLogo} alt="" width="100%" />
                         <div className="mt-4">
                             <span className="fw-semibold cronnos-font-12">Preencha os campos indicados para realizar seu cadastro em nossa plataforma</span>
                         </div>
@@ -28,6 +29,19 @@ const Cadastro = () => {
                                 <div className="cronnos-font-11 text-muted mt-2">Informe um email para cadastro</div>
                             </div>
                             <div className="mb-1">
+                                <label className="form-label cronnos-font-12 fw-semibold">Tipo</label>
+                                <select class="form-select form-select-sm border-primary" aria-label="Default select example">
+                                    <option value="1">Física</option>
+                                    <option value="2">Jurídica</option>
+                                </select>
+                                <div className="cronnos-font-11 text-muted mt-2">Selecione o tipo de pessoa Física ou Jurídica</div>
+                            </div>
+                            <div className="mb-4">
+                                <label className="form-label cronnos-font-12 fw-semibold">Nome</label>
+                                <input type="text" className="form-control form-control-sm border-primary" placeholder="João Silva ou Logistica LTDA"/>
+                                <div className="cronnos-font-11 text-muted mt-2">Seu nome ou de sua organização</div>
+                            </div>
+                            <div className="mb-1">
                                 <label className="form-label cronnos-font-12 fw-semibold">Senha</label>
                                 <input type="password" className="form-control form-control-sm border-primary" />
                                 <div className="cronnos-font-11 text-muted mt-2">Sua senha deve conter ao menos 6 caracteres</div>
@@ -37,6 +51,11 @@ const Cadastro = () => {
                                 <input type="password" className="form-control form-control-sm border-primary" />
                             </div>
                             <button type="submit" className="btn btn-primary btn-sm w-100">Cadastrar</button>
+                            <div className="cronnos-font-11 text-center">
+                                <div className="mt-2 fw-semibold">
+                                    <span>Já possui cadastro e esqueceu seus dados?</span> <Link to="/cadastro">Click aqui</Link>
+                                </div>
+                            </div>
                         </form>
                     </div>
                 </div>
