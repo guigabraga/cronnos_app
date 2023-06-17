d1171629754b2346d9c9ee28f899a839942c4a7e
Guilherme Braga <guilherme_braga100@hotmail.com>
2023-06-17 01:41:17 +0000

    Organização de código

--- src/components/Dashboard/UltimosLogs.jsx
+++ src/components/Dashboard/UltimosLogs.jsx
@@ -4,24 +4,39 @@ import Axios from 'axios'
 import LoadSpinner from '../LoadSpinner'
 
 function UltimosLogs(){
+
     const [removeLoadSpinner, setRemoveLoadSpinner] = useState(true)
     const [renderTable, setRenderTable] = useState([])
-    useEffect(function SelectLogs(){
+
+    useEffect(() => {
+
         setInterval(() => {
+
             Axios.get('http://31.220.31.209:5001/select-logs', {
+
                 timeout: 10000
+
             })
+
             .then(function (response) {
+
                 const dataSelectLogs = [response.data.data.data]
                 setRenderTable(dataSelectLogs[0])
                 setRemoveLoadSpinner(false)
+
             })
+
             .catch(function (error) {
+
                 console.log(error)
                 setRemoveLoadSpinner(false)
+
             })
+
         }, 1000)
+
     }, [])
+    
     return(
         <div className='shadow-sm rounded border p-3 m-1'>
             <span className='fw-semibold cronnos-font-14'>Últimos Logs de sistema</span>
